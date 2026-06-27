eec64b8853ae914136c7bfef9d36d08002d689a7
michal-skraburski <michal@skraburski.com>
2026-06-27 22:06:48 +0100

    change config to use monorepo paths

--- image/moodle-docker/quick-start.sh
+++ image/moodle-docker/quick-start.sh
@@ -1,7 +1,7 @@
 # Change ./moodle to your /path/to/moodle if you already have it checked out
 export MOODLE_DOCKER_WWWROOT=./moodle
-export CODERUNNER_REPO=../moodle-qtype_coderunner/
-export CR_DEPEND_REPO=../moodle-qbehaviour_adaptive_adapted_for_coderunner/
+export CODERUNNER_REPO=../../deps/moodle-qtype_coderunner/
+export CR_DEPEND_REPO=../../deps/moodle-qbehaviour_adaptive_adapted_for_coderunner/
 # Choose a db server (Currently supported: pgsql, mariadb, mysql, mssql, oracle)
 export MOODLE_DOCKER_DB=pgsql
 export MOODLE_DOCKER_WEB_HOST=192.168.100.2
