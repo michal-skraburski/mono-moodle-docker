7992c4be7c438cf2b9fcb471248a9727f4cf84c6
michal-skraburski <michal@skraburski.com>
2026-06-27 21:51:00 +0100

    setup system for local work, remove dependency on local jobeinbox image

--- quick-start.sh
+++ quick-start.sh
@@ -4,6 +4,7 @@ export CODERUNNER_REPO=../moodle-qtype_coderunner/
 export CR_DEPEND_REPO=../moodle-qbehaviour_adaptive_adapted_for_coderunner/
 # Choose a db server (Currently supported: pgsql, mariadb, mysql, mssql, oracle)
 export MOODLE_DOCKER_DB=pgsql
+export MOODLE_DOCKER_WEB_HOST=192.168.100.2
 
 # Get Moodle code, you could select another version branch (skip this if you already got the code)
 git clone -b v4.5.4 git://git.moodle.org/moodle.git $MOODLE_DOCKER_WWWROOT
