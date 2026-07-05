f0c2afc6cde0713976d11fa83a12c684b8e1a7db
michal-skraburski <michal@skraburski.com>
2026-07-05 21:49:02 +0100

    add sh for compiling the amd files in cr

---
+++ image/moodle-docker/compile-amd-coderunner.sh
@@ -0,0 +1,8 @@
+export MOODLE_DOCKER_WWWROOT=./moodle
+export CODERUNNER_REPO=../../deps/moodle-qtype_coderunner/
+export CR_DEPEND_REPO=../../deps/moodle-qbehaviour_adaptive_adapted_for_coderunner/
+# Choose a db server (Currently supported: pgsql, mariadb, mysql, mssql, oracle)
+export MOODLE_DOCKER_DB=pgsql
+export MOODLE_DOCKER_WEB_HOST=192.168.100.2
+
+./bin/moodle-docker-grunt-coderunner
