8eccaa9b2c085aa35030fc504e7e8235ce91e750
michal-skraburski <michal@skraburski.com>
2026-07-08 21:47:02 +0100

    add phpunit tester

--- image/moodle-docker/phpunit.sh
+++ image/moodle-docker/phpunit.sh
@@ -1,3 +1,4 @@
+# Change ./moodle to your /path/to/moodle if you already have it checked out
 export MOODLE_DOCKER_WWWROOT=./moodle
 export CODERUNNER_REPO=../../deps/moodle-qtype_coderunner/
 export CR_DEPEND_REPO=../../deps/moodle-qbehaviour_adaptive_adapted_for_coderunner/
@@ -5,4 +6,6 @@ export CR_DEPEND_REPO=../../deps/moodle-qbehaviour_adaptive_adapted_for_coderunn
 export MOODLE_DOCKER_DB=pgsql
 export MOODLE_DOCKER_WEB_HOST=192.168.100.2
 
-./bin/moodle-docker-grunt-coderunner "$@"
+bin/moodle-docker-compose exec webserver php admin/tool/phpunit/cli/init.php
+
+bin/moodle-docker-compose exec webserver vendor/bin/phpunit --verbose question/type/coderunner/tests/docs_page_test.php
