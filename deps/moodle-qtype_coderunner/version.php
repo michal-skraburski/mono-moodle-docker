acde1e2ef3c6b1581566259a7abce16be7c04b49
michal-skraburski <michal@skraburski.com>
2026-07-22 11:38:58 +0200

    bump to 5.10.0

--- deps/moodle-qtype_coderunner/version.php
+++ deps/moodle-qtype_coderunner/version.php
@@ -22,12 +22,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2026062300;
+$plugin->version  = 2026072200;
 $plugin->requires = 2023100900; // Moodle 4.3.0 or later.
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '5.9.3';
+$plugin->release = '5.10.0';
 
 $plugin->dependencies = [
     'qbehaviour_adaptive_adapted_for_coderunner' => 2026051000,
